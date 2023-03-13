2cb6caa02f9070c2cccfd8f2dc18b3b37b67b84d
Umair Jibran <me@umairjibran.com>
2023-03-13 21:22:32 +0500

    try catch

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-function addNumbers(number1, number2) {
-  const sum = number1 + number2;
-  return sum;
+try {
+  throw new Error('TESTING');
+} catch (err) {
+  console.error(err);
 }
-
-console.log(process.argv);
-
-const number1 = parseFloat(process.argv[2]);
-const number2 = parseFloat(process.argv[3]);
-
-const result = addNumbers(number1, number2);
-
-console.log(result);
