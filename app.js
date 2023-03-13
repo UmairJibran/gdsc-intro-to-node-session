87ef5fb911001b4448d1e40a99f7a2affd7801d9
Umair Jibran <me@umairjibran.com>
2023-03-13 22:22:05 +0500

    basic express server

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
-try {
-  throw new Error('TESTING');
-} catch (err) {
-  console.error(err);
+// import express package from the node_modules
+const Express = require('express');
+
+// initialize app as Express instance
+const app = Express();
+
+app.use(Express.json({ limit: '100mb' }));
+
+app.get('/', (req, res) => {
+  try {
+    const { language } = req.query;
+    let responseMessage = '';
+    if (language === 'ur') {
+      responseMessage = 'Kaamyab';
+    } else {
+      responseMessage = 'Success';
+    }
+    return res.status(200).json({
+      message: responseMessage,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: 'An Error Occurred',
+    });
+  }
+});
+
+app.post('/add-two-numbers', (req, res) => {
+  try {
+    const { number1, number2 } = req.body;
+
+    const result = addNumbers(number1, number2);
+
+    return res.status(200).json({
+      message: result,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: 'An Error Occurred',
+    });
+  }
+});
+
+// export the newly intialized app
+module.exports = app;
+
+function addNumbers(num1, num2) {
+  return num1 + num2;
 }
---
+++ server.js
@@ -0,0 +1,5 @@
+const justAVariable = require('./app');
+
+justAVariable.listen(8080, () => {
+  console.log('OUR SERVER IS RUNNING ON PORT 8080');
+});
